c5ca660dc8255c5d6434ca5d3d19bb7bb5eb3749
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-10 14:02:44 -0800

    search routes for specific show

--- controllers/charities.js
+++ controllers/charities.js
@@ -2,29 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Code = require('../models/charity');
 const unirest = require('unirest');
-// const apiKey = "oFuGss0yoxmshRmwtTTloScfpjxxp1Fr3oGjsn2PW3x7xFCqxO" //Utelly
-const apiKey = "3b51888e8cf75b14ee2d39b1b5e47e59959fbf20" //Guidebox
-// get the models required in here...
+const apiKey = "3b51888e8cf75b14ee2d39b1b5e47e59959fbf20" //Guidebox api key
 
 // "http://api-public.guidebox.com/v2/{endpoint}"
 // "https://api.guidebox.com/docs"
 
 // INDEX
-// router.get('/', (req, res) => {
-//   unirest.get("https://utelly-tv-shows-and-movies-availability-v1.p.mashape.com/lookup?country=us&term=elementary")
-//     .header("X-Mashape-Key", apiKey)
-//     .header("Accept", "application/json")
-//     .end(function (result) {
-//       res.send(result.body);
-//   });
-// });
-
 router.get('/', (req, res) => {
-  unirest.get("http://api-public.guidebox.com/v2/shows?channel=hbo")
+  unirest.get("http://api-public.guidebox.com/v2/search?type=show&field=title&query=elementar")
     .header("Authorization", apiKey)
     .end(function (result) {
-      res.send(result.body);
+      var id = String(result.body.results[0].id);
+      res.redirect(`/results/${id}`)
   });
 });
+
+router.get('/results/:id', (req, res) => {
+  unirest.get(`http://api-public.guidebox.com/v2/shows/${Number(req.params.id)}/available_content`)
+  .header("Authorization", apiKey)
+  .end(function (result) {
+    res.send(result.body);//.results.web.episodes.all_sources)
+  });
+})
 
 module.exports = router
