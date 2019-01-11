3fd6beb420ce73304c5a334232b3ecc3ab821fce
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-10 17:32:46 -0800

    added search funtionality

--- controllers/charities.js
+++ controllers/charities.js
@@ -2,27 +2,97 @@ const express = require('express');
 const router = express.Router();
 const Code = require('../models/charity');
 const unirest = require('unirest');
-const apiKey = "3b51888e8cf75b14ee2d39b1b5e47e59959fbf20" //Guidebox api key
+const apiKey = "3b51888e8cf75b14ee2d39b1b5e47e59959fbf20"; //Guidebox api key
+var resultArray = [];
+var resultsArray = [];
+var fullResults = [];
 
 // "http://api-public.guidebox.com/v2/{endpoint}"
 // "https://api.guidebox.com/docs"
 
+router.get('/callsleft', (req, res) => {
+  unirest.get("http://api-public.guidebox.com/v2/quota")
+  .header("Authorization", apiKey)
+  .end(function (result) {
+    res.send(result)
+  })
+})
+
+router.get('/', (req,res) => {
+  res.render('index')
+})
+
 // INDEX
-router.get('/', (req, res) => {
-  unirest.get("http://api-public.guidebox.com/v2/search?type=show&field=title&query=elementar")
+router.post('/search', (req, res, next) => {
+  var qstring = req.body.qstring
+  unirest.get(`http://api-public.guidebox.com/v2/search?type=show&field=title&query=${qstring}`)
     .header("Authorization", apiKey)
     .end(function (result) {
-      var id = String(result.body.results[0].id);
-      res.redirect(`/results/${id}`)
+      for(i=0; i<result.body.results.length; i++){
+        var id = String(result.body.results[i].id)
+        resultArray.push(id)
+      }
+      // next()
+      // res.redirect('/showall')
+      res.send(`finished searching for ${qstring}`)
   });
 });
 
-router.get('/results/:id', (req, res) => {
-  unirest.get(`http://api-public.guidebox.com/v2/shows/${Number(req.params.id)}/available_content`)
-  .header("Authorization", apiKey)
-  .end(function (result) {
-    res.send(result.body);//.results.web.episodes.all_sources)
-  });
+// router.get('/search', (req, res, next) => {
+//   unirest.get("http://api-public.guidebox.com/v2/search?type=movie&field=title&query=elementary")
+//     .header("Authorization", apiKey)
+//     .end(function (result) {
+//       for(i=0; i<result.body.results.length; i++){
+//         var id = String(result.body.results[i].id)
+//         resultArray.push(id)
+//       }
+//       // res.send(resultArray)
+//       console.log(resultArray)
+//       // res.redirect('/results')
+//       next()
+//   })
+// })
+
+
+router.get('/showall', (req, res)=> {
+  for(i=0; i<resultArray.length; i++){
+    // router.get(`/results/${Number(resultArray[i])}`, (req, res, next) => {
+      unirest.get(`http://api-public.guidebox.com/v2/shows/${Number(resultArray[i])}/available_content`)
+      .header("Authorization", apiKey)
+      .end(function (result) {
+        // res.send(result.body);//.results.web.episodes.all_sources)
+        for(j=0; j<result.body.results.web.episodes.all_sources.length; j++){
+          resultsArray.push(result.body.results.web.episodes.all_sources[j].display_name)
+          // console.log(result.body.results.web.episodes.all_sources[j])
+        }
+        // resultsArray.push(result.body.results.web.episodes.all_sources)
+        // console.log(resultsArray)
+        // console.log(result.body);//.results.web.episodes.all_sources)
+        // res.send(resultsArray)
+        // console.log(resultArray[i])
+        // next()
+      });
+
+    // })
+    // res.send("helo")
+    // res.redirect('/results')
+
+  }
+  for(i=0; i<resultArray.length; i++){
+    fullResults.push(resultArray[i])
+    fullResults.push(resultsArray[i])
+  }
+  // fullResults.push()
+  res.redirect('/results')
+})
+
+router.get('/results', (req, res) => {
+  // res.send(resultsArray)
+  console.log("last")
+  console.log(fullResults)
+  // res.send("done")
+  // res.send(fullResults)
+  res.status(200).send([String(resultArray), resultsArray])
 })
 
 module.exports = router
--- public/js/app.js
+++ public/js/app.js
@@ -12,3 +12,14 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "demo_get.asp", true);
+  xhttp.send();
+}
