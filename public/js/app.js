11a6269275c700e28db8db77751396d64b661d5b
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-22 13:57:36 -0800

    custom css init

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,25 @@
 
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "demo_get.asp", true);
-  xhttp.send();
-}
+// window.onscroll = function() {myFunction()};
+//
+// var header = document.getElementById("myHeader");
+//
+// var sticky = header.offsetTop;
+//
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
+//
+// function loadDoc() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById("demo").innerHTML = this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "demo_get.asp", true);
+//   xhttp.send();
+// }
